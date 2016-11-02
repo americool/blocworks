30c2fc4e6435957799c4dda6e448de0f91ebd89d
Americool <aberanderson@gmail.com>
2016-11-02 14:25:13 -0400

    finish checkpoint/assignment3

--- bloc-books/app/controllers/books_controller.rb
+++ bloc-books/app/controllers/books_controller.rb
@@ -3,4 +3,16 @@ class BooksController < BlocWorks::Controller
     @book = "Eloquent Ruby"
     render(:welcome)#, book: "Eloquent Ruby"
   end
+
+  def index
+    @books = Book.all
+    render(:index)
+  end
+
+  def show
+    data = Rack::Request.new(@env)
+    id = data.params["id"].to_i
+    @book = Book.find_one(id)
+    render(:show)
+  end
 end
